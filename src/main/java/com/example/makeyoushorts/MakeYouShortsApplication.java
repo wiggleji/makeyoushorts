27b7a1b611d1c72e12836b7808013ad58b787bb5
wiggleji <roamgom@gmail.com>
2023-08-30 21:11:34 +0900

    Init SpringBoot

---
+++ src/main/java/com/example/makeyoushorts/MakeYouShortsApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeyoushorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeYouShortsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeYouShortsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makeyoushorts/MakeYouShortsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeyoushorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeYouShortsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
