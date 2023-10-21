97981f315bf96cc8b11904462ee6ac292981fd27
wiggleji <roamgom@gmail.com>
2023-10-21 23:58:36 +0900

    [feat] add video_downloader & file_manager for video serve service

---
+++ videoDownloader/file_manager.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+from typing_extensions import deprecated
+
+
+class FileManager:
+    # output: videoDownloader/temp
+    DEFAULT_VIDEO_FILE_TEMP_PATH = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "temp"
+    )
+
+    def video_file_exists(self, filename):
+        file_exists = os.path.isfile(
+            os.path.join(self.DEFAULT_VIDEO_FILE_TEMP_PATH, f"{filename}.mp4")
+        )
+        return file_exists
+
+    @deprecated("use to delete temp files on server with celery worker")
+    def delete_video_file(self, filename):
+        if self.video_file_exists(filename):
+            os.remove(
+                os.path.join(self.DEFAULT_VIDEO_FILE_TEMP_PATH, f"{filename}.mp4")
+            )
+            return True
+        return False
---
+++ videoDownloader/test/test_file_manager.py
@@ -0,0 +1,27 @@
+import pytest
+
+from file_manager import FileManager
+from unittest import TestCase
+
+
+class FileManagerTest(TestCase):
+    @pytest.fixture(autouse=True)
+    def fixture(self):
+        self.filemanager = FileManager()
+        self.sample_video_name = "sample_video"
+
+    def test_video_file_exists__returns_True__file_exists(self):
+        # given
+
+        # when
+
+        # then
+        self.assertTrue(self.filemanager.video_file_exists(self.sample_video_name))
+
+    def test_video_file_exists__returns_False__file_not_exists(self):
+        # given
+
+        # when
+
+        # then
+        self.assertFalse(self.filemanager.video_file_exists("INVALID"))
